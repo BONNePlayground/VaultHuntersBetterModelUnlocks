fd1e88e13d34dbd1d33269d678841732af4c735a
BONNe <bonne@bonne.id.lv>
2023-11-03 08:39:58 +0200

    Fixes gear identification from backpacks obfuscation issue.

--- src/main/java/lv/id/bonne/vaulthunters/bettermodelunlocks/vaulthunters/mixin/MixinIdentificationStandBlock.java
+++ src/main/java/lv/id/bonne/vaulthunters/bettermodelunlocks/vaulthunters/mixin/MixinIdentificationStandBlock.java
@@ -22,20 +22,18 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.p3pp3rf1y.sophisticatedbackpacks.api.CapabilityBackpackWrapper;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackItem;
-import net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems;
 import net.p3pp3rf1y.sophisticatedcore.inventory.ITrackedContentsItemHandler;
 
 
 /**
  * This mixin allows to identify vault items inside backpack by clicking on identification stand.
  */
-@Mixin(value = IdentificationStandBlock.class, remap = false)
+@Mixin(value = IdentificationStandBlock.class)
 public class MixinIdentificationStandBlock
 {
     /**
